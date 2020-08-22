c241242c499e3e2bcd8cc203f289c6490e75e605
Paul Borawski <paulborawski14@gmail.com>
2020-08-22 10:30:47 -0400

    added websocket component

---
+++ client/src/components/WebsocketComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class WebsocketComp extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default WebsocketComp;
