83dff5b38ff8874cfabff1decf37f454adca6657
Paul Borawski <paulborawski14@gmail.com>
2020-08-22 10:20:56 -0400

    you need a websocket clinet

--- flasker.py
+++ flasker.py
@@ -1,6 +1,8 @@
 from geventwebsocket.handler import WebSocketHandler
 from gevent.pywsgi import WSGIServer
 from flask import Flask, request, render_template
+import asyncio
+import websockets
 
 app = Flask(__name__)
 
@@ -17,6 +19,10 @@ def api():
             ws.send(message)
     return
 
-if __name__ == '__main__':
-    http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
-    http_server.serve_forever()
+#if __name__ == '__main__':
+#    http_server = WSGIServer(('',5000), app, handler_class=WebSocketHandler)
+#    http_server.serve_forever()
+
+start_server = websockets.serve(app, "localhost", 8765)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
